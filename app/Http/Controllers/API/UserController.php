0e275516a8b0e095470f6e8288d8f7441284490b
Muhammad Ali Akbar <maakbarofficial@gmail.com>
2025-11-14 07:47:14 +0500

    Add user creation api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,48 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
-    //
+    public function createUser(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation Error',
+                'data' => null,
+                'error' => $validator->errors()
+            ], 400);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        if ($user->id) {
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'data' => $user
+            ], 200);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'Something went wrong',
+            'data' => null
+        ], 400);
+    }
 }
